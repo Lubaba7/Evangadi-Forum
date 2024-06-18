e70a3a257b1bf0c91e8efefebda35dcce3b653ea
Saada0 <lubaba980@gmail.com>
2024-06-18 14:49:17 -0700

    minor change

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const questionRoutes = require("./routes/questionRoute");
 const answerRoutes = require("./routes/answerRoute");
 const authMiddleware = require("./middleware/authMiddlewares");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 // Enable CORS for all routes
