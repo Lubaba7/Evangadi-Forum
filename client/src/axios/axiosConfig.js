590905de872d493d74cfcf4427ac1b97813d312b
Saada0 <lubaba980@gmail.com>
2024-06-18 14:44:45 -0700

    Change local host to remote server

--- client/src/axios/axiosConfig.js
+++ client/src/axios/axiosConfig.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+
+  // Remote server
+  baseURL: "https://evangadi-forum-qqkm.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
