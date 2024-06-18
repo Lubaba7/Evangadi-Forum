7881825b8c7c1fd0a3bd3ceab966647a9f108428
Saada0 <lubaba980@gmail.com>
2024-06-18 14:54:13 -0700

    change server address

--- client/src/axios/axiosConfig.js
+++ client/src/axios/axiosConfig.js
@@ -3,7 +3,7 @@ const axiosBase = axios.create({
   // baseURL: "http://localhost:5000/api",
 
   // Remote server
-  baseURL: "https://evangadi-forum-qqkm.onrender.com/api",
+  baseURL: "https://evangadi-forum-l9c6.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
